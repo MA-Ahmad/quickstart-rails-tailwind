689d764ce3ba1bbeb11db7bf575a15f3239f180f
shakeelphaedra <shakeel@phaedrasolutions.com>
2021-04-26 15:11:02 +0500

    added tailwindcss

---
+++ app/frontend/stylesheets/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    "./app/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/frontend/**/*.js",
+    "./app/frontend/**/*.vue",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
+    require("tailwindcss")("./app/frontend/stylesheets/tailwind.config.js"),
+    require("postcss-import"),
+    require("postcss-flexbugs-fixes"),
+    require("postcss-preset-env")({
       autoprefixer: {
-        flexbox: 'no-2009'
+        flexbox: "no-2009",
       },
-      stage: 3
-    })
-  ]
-}
+      stage: 3,
+    }),
+  ],
+};

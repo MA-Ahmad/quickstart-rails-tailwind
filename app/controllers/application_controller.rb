c478e6ad9d37617b0782f1320aefaf1a4cebd088
shakeelphaedra <shakeel@phaedrasolutions.com>
2021-04-27 17:00:41 +0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
 
+    before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
+
+    # Overwriting the sign_out redirect path method
+    def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
+    end
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])

d962400535a3ea13f43ad76b2947653b0575db74
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-05 10:19:34 -0700

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+    before_action :authenticate_user!, only: :account
+
+    def account
+        @user = current_user
+    end
+
     def about; end
 
     def team; end

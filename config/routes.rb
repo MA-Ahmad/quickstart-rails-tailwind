50bef42045b4917381c375f93c718653f5ebb1d9
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-05 10:19:10 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
-  resources :users, only: :show
 
   scope :page do
     get '/team', to: 'pages#team'
     get '/features', to: 'pages#features'
     get '/projects', to: 'pages#projects'
+    get '/settings/account', to: 'pages#account'
   end
+
+  devise_for :users
+  resource :user, only: :update
+  get :profile, to: 'users#show'
+  patch 'profile/password/update', to: 'users#update_password', as: :update_password
 end

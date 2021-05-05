ca2deeb607473fcb08123dacf27e7a95c193b8f2
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-05 10:19:27 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :trackable, :rememberable, :validatable
+
+  def update_password_with_password(params, *options)
+    current_password = params.delete(:current_password)
+    result = if valid_password?(current_password)
+               update(params, *options)
+             else
+               self.assign_attributes(params, *options)
+               self.valid?
+               self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+               false
+             end
+    result
+  end
 end
